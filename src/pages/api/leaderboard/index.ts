7932953b87145aaf4b05955bda245746615fc791
Sliminconcoova <ollieclees@gmail.com>
2025-07-22 10:30:39 +1000

    another temp fix for missing points feature

--- src/pages/api/leaderboard/index.ts
+++ src/pages/api/leaderboard/index.ts
@@ -27,7 +27,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       }));
 
       // Sort descending by totalPoints
-      formatted.sort((a, b) => b.totalPoints - a.totalPoints);
+      //formatted.sort((a, b) => b.totalPoints - a.totalPoints);
 
       return res.status(200).json(formatted);
     } catch (error) {
