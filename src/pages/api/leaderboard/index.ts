a2c7668fdced1b4af0b9bbd7e5889166570454a9
Sliminconcoova <ollieclees@gmail.com>
2025-07-22 10:26:45 +1000

    Remove predictions on leaderboard

--- src/pages/api/leaderboard/index.ts
+++ src/pages/api/leaderboard/index.ts
@@ -13,7 +13,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
           predictions: {
             select: {
               //points: true,
-              predictions: true
             },
           },
         },
