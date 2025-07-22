68cf369c96e142cd36acdb8fb5212b35f71f4671
Sliminconcoova <ollieclees@gmail.com>
2025-07-22 11:24:05 +1000

    Added login page contents

---
+++ src/pages/login.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function LoginPage() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Login</h1>
+      <p>Login form will go here.</p>
+    </div>
+  );
+}
